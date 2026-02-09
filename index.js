c4b0c94313105c31bdad94e37f82f63da0c76361
robertjacques556 <robertjacques556@gmail.com>
2026-02-09 23:17:00 +0000

    adiciona rota /personagens para retornar lista de personagens

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.get('/oi', (req, res) => {
 })
 
 
+ const lista =['Rick Sanchez', 'Morty Smith', 'Beth Smith']
+
+app.get('/personagens', (req, res) => {
+    res.json(lista)
+  
+})  
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
