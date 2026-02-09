9df446405fb58bb2bdd5ae797a737c2d2764f833
robertjacques556 <robertjacques556@gmail.com>
2026-02-09 23:35:14 +0000

    adiciona rota para retornar personagem específico pelo ID

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ app.get('/personagens', (req, res) => {
     res.json(lista)
   
 })  
+app.get('/personagens/:id', (req, res) => {
+    const id = req.params.id
+    const personagem = lista[id-1]
+    res.send(personagem)
+  
+})
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
